4579b9bbf50aedd957689a11c91ec01ffef5bc45
Aspiring Astro <aspiring.astro@gmail.com>
2020-09-27 20:11:21 -0700

    Add redirect support

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
 bundle exec jekyll build
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  echo build success
+  rsync -avP _site/ docs
+else
+  echo build failed
+fi
+
---
+++ docs/docs/docs/assets/javascripts/application.js
@@ -0,0 +1,46 @@
+function updateFixedness() {
+  var scrolledDown   = window.pageYOffset > 0;
+  var mobileMenuOpen = document.getElementById('nav-trigger').checked;
+  const header       = document.getElementById("site-header");
+
+  if (!header) {return;}
+
+  if(scrolledDown || mobileMenuOpen) {
+    header.classList.add("fixed");
+  } else {
+    header.classList.remove("fixed");
+  }
+}
+
+window.addEventListener('scroll', updateFixedness);
+
+window.addEventListener('load', function() {
+  document.getElementById('nav-trigger')
+          .addEventListener('change', updateFixedness);
+});
+
+window.addEventListener('load', function() {
+
+  function bindSocialClick(selector, url) {
+    var element = document.querySelector(selector);
+    if (!element) {return;}
+
+    element.addEventListener('click', function() {
+      var left = (screen.width - 570) / 2;
+      var top = (screen.height - 570) / 2;
+      var params = "menubar=no,toolbar=no,status=no,width=570,height=570,top=" + top + ",left=" + left;
+      window.open(url,"NewWindow",params);
+    });
+  }
+
+  var pageUrl = encodeURIComponent(document.URL);
+
+  bindSocialClick('.social-media-buttons .instagram', 'https://www.instagram.com/sharer/sharer.php?u=' + pageUrl);
+  bindSocialClick('.social-media-buttons .facebook', 'https://www.facebook.com/sharer/sharer.php?u=' + pageUrl);
+  bindSocialClick('.social-media-buttons .twitter',  'https://twitter.com/intent/tweet?url=' + pageUrl + '&text=' + encodeURIComponent('TimeTurner.Today'));
+  bindSocialClick('.social-media-buttons .linkedin', 'https://www.linkedin.com/shareArticle?mini=true&url=' + pageUrl);
+  bindSocialClick('.follow-us-on-twitter',           'https://twitter.com/intent/follow?screen_name=todaytimeturner');
+  bindSocialClick('.follow-us-on-twitter.english',   'https://twitter.com/intent/follow?screen_name=todaytimeturner');
+
+});
+
